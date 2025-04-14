70b47d7d959808350320f328fde69d9ca0ce5dab
AliSepar <alisabah.separ1@gmail.com>
2025-04-14 17:30:39 +0200

    creating components

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import appwriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
+
+// its appwrite thing that need $ in the id variable
+function PostCard({ $id, title, featuredIMage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={appwriteService.getFilePreview(featuredIMage)}
+            // the featuredImage is the id of image which is stored in the post table so to get the image we
+            // the getFilePreview to give the path of the image stored in storage
+            alt="post image"
+            className="rounded-xl"
+          />
+        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,23 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./ui/Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./ui/Input";
+import SelectInput from "./ui/SelectInput";
+import PostCard from "./PostCard";
+import Button from "./ui/Button";
+import AuthLayout from "./AuthLayout";
+import SignUp from "./SignUp";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  SelectInput,
+  PostCard,
+  Button,
+  AuthLayout,
+  SignUp,
+};
